fcc252cbc845e6861f4a2a2769e2c9e7258612f3
SergeyG28 <swgs2895@gmail.com>
2026-06-08 21:08:56 +0500

    Author, Genre, new models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
